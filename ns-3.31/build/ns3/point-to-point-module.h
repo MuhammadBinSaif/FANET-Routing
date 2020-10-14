dd8cbc7cbad49b407fc0b428cb83a1e207861376
saif <saifisra0098@gmail.com>
2020-10-14 18:17:41 +0500

    Build Files

---
+++ ns-3.31/build/ns3/point-to-point-module.h
@@ -0,0 +1,14 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_POINT_TO_POINT
+    
+
+// Module headers:
+#include "point-to-point-channel.h"
+#include "point-to-point-helper.h"
+#include "point-to-point-net-device.h"
+#include "ppp-header.h"
+#endif
---
+++ ns-3.31/build/ns3/stats-config.h
@@ -0,0 +1,14 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_NS3_STATS_CONFIG_H_WAF
+#define W_NS3_STATS_CONFIG_H_WAF
+
+#define HAVE_SYS_IOCTL_H 1
+#define HAVE_IF_NETS_H 1
+#define HAVE_NET_ETHERNET_H 1
+#define HAVE_IF_TUN_H 1
+#define HAVE_PACKET_H 1
+#define HAVE_SQLITE3 1
+#define HAVE_SEMAPHORE_H 1
+
+#endif /* W_NS3_STATS_CONFIG_H_WAF */
